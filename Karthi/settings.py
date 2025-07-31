109c1ad0ef001957ec793cda6056f09331a233aa
karthikeyansa8 <karthikeyansa8@gmail.com>
2025-07-31 15:48:55 +0530

    dummy

--- Karthi/settings.py
+++ Karthi/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql', #postgresql
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3', 
         'NAME':  config('DB_NAME'),
         'USER': config('USER'),
         'PASSWORD': config('PASSWORD'),
